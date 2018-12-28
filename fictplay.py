e3253b3786d209d87e3f33345c8d335ad212ea32
Yuya Furusawa <fullyou0798@gmail.com>
2018-12-28 21:41:23 +0900

    n player game

--- fictplay.py
+++ fictplay.py
@@ -9,58 +9,63 @@ Fictitious play model.
 from __future__ import division
 
 import numpy as np
-from normal_form_game import NormalFormGame, pure2mixed
+import copy
+from normal_form_game import NormalFormGame, pure2mixed, Player
 
 
 class FictitiousPlay(object):
     """
-    Fictitious play with two players
+    Class representing the Fictitious play model with two players
 
     Parameters
     ----------
-    data : array_like(float) or NormalFormGame
-
+    data : array_like(float) or `NormalFormGame` object
+        The payoff matrix of the two-player game
 
     Attributes
     ----------
-    g : NormalFormGame
+    g : `NormalFormGame` object
 
-    players : list(Player)  # tuple
+    N : scalar(int)
+        Number of players
+
+    players : tuple(Player)
+        tuple of Player instances
 
     nums_actions : tuple(int)
-
-    current_beliefs : tuple(ndarray(float, ndim=1))
+        Number of actions for each player
 
     """
+
     def __init__(self, data):
         if isinstance(data, NormalFormGame):
-            if data.N != 2:
-                raise ValueError('input game must be a two-player game')
             self.g = data
         else:  # data must be array_like
             payoffs = np.asarray(data)
-            if not (payoffs.ndim in [2, 3]):
-                raise ValueError(
-                    'input data must be a square matrix or a bimatrix'
-                )
             self.g = NormalFormGame(payoffs)
 
-        self.N = self.g.N  # Must be 2
+        self.N = self.g.N 
         self.players = self.g.players
         self.nums_actions = self.g.nums_actions
         self.tie_breaking = 'smallest'
 
         self.current_actions = np.zeros(self.N, dtype=int)
 
-        self.belief_sizes = tuple(
-            self.nums_actions[1-i] for i in range(self.N)
-        )
-        # Create instance variable `current_belief` for self.players
-        for player, belief_size in zip(self.players, self.belief_sizes):
-            player.current_belief = np.empty(belief_size)
+        # Create the list of opponents for each player
+        self.opponent_list = list(list(range(self.N)) for i in range(self.N))
+        for i in range(self.N):
+            self.opponent_list[i].remove(i)
 
-        self._decreasing_gain = lambda t: 1 / (t+1)
-        self.step_size = self._decreasing_gain
+        self.assessment_sizes = tuple(
+                tuple(self.nums_actions[j] for j in self.opponent_list[i]) for i in range(self.N)
+                )
+        
+        # Create instance variable `current_assessment` for self.players
+        for i, player in enumerate(self.players):
+            player.current_assessment = np.asarray([np.empty(self.nums_actions[j]) for j in self.opponent_list[i]])
+
+        self._initial_weight = lambda t: 1 / (t+1)
+        self.step_size = self._initial_weight
 
     def __repr__(self):
         msg = "Fictitious play for "
@@ -71,80 +76,148 @@ class FictitiousPlay(object):
     def __str__(self):
         return self.__repr__()
 
+    @property
+    def current_assessments(self):
+        return tuple(player.current_assessment for player in self.players)
+
     def set_init_actions(self, init_actions=None):
+        """
+        Randomly sets `current_actions` if `init_actions` is None.
+        And initialize `current_assessment` for each player given `current_actions`
+        """
         if init_actions is None:
             init_actions = np.zeros(self.N, dtype=int)
             for i, n in enumerate(self.nums_actions):
                 init_actions[i] = np.random.randint(n)
         self.current_actions[:] = init_actions
 
-        # Initialize current_belief for each player
         for i, player in enumerate(self.players):
-            player.current_belief[:] = \
-                pure2mixed(self.belief_sizes[i], init_actions[1-i])
-
-    @property
-    def current_beliefs(self):
-        return tuple(player.current_belief for player in self.players)
+            for j in self.opponent_list[i]:
+                if j>i:
+                    k = j - 1
+                player.current_assessment[k][:] = \
+                    pure2mixed(self.assessment_sizes[i][k], init_actions[j])
 
     def play(self):
+        """
+        The method used to proceed the game by one period. Players take their
+        best response strategies given assessments about opponents actions.
+
+        """
         for i, player in enumerate(self.players):
             self.current_actions[i] = \
-                player.best_response(player.current_belief,
-                                     tie_breaking=self.tie_breaking)
+                player.best_response(player.current_assessment if self.N > 2 else player.current_assessment[0],
+                                    tie_breaking=self.tie_breaking)
 
-    def update_beliefs(self, step_size):
+    def update_asseccements(self, step_size):
+        """
+        This method used to update each players' assessments. Assessments are updated by
+        opponent's action and `step_size`.
+
+        """
         for i, player in enumerate(self.players):
-            # x[i] = x[i] + step_size * (a[1-i] - x[i])
-            #      = (1-step_size) * x[i] + step_size * a[1-i]
-            # where x[i] = player's current_belief,
-            #       a[1-i] = opponent's current_action.
-            player.current_belief *= 1 - step_size
-            player.current_belief[self.current_actions[1-i]] += step_size
+            for j in self.opponent_list[i]:
+                if j > i:
+                    k = j - 1
+                player.current_assessment[k][:] *= 1 - step_size
+                player.current_assessment[k][self.current_actions[j]] += step_size
 
-    def simulate(self, ts_length, init_actions=None):
-        beliefs_sequence = np.empty((ts_length, sum(self.nums_actions)))
-        beliefs_iter = self.simulate_iter(ts_length, init_actions)
+    def _sum_index(self, i, j):
+        #i and j are index
+        total = 0
+        if i==0:
+            if j==0:
+                total = 0
+            else:
+                for k in range(j):
+                    total += self.assessment_sizes[0][k]
+        else:
+            for k in range(i):
+                total += sum(self.assessment_sizes[k])
+            for l in range(j):
+                total += self.assessment_sizes[i][l]
+        return total
 
-        for t, beliefs in enumerate(beliefs_iter):
-            (beliefs_sequence[t, :self.belief_sizes[0]],
-             beliefs_sequence[t, self.belief_sizes[0]:]) = beliefs
+    def get_time_series(self, ts_length, init_actions=None):
+        """
+        Return the assessments of each players in each round.
 
-        return (beliefs_sequence[:, :self.belief_sizes[0]],
-                beliefs_sequence[:, self.belief_sizes[0]:])
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            The number of rounds you play in a simulation.
 
-    def simulate_iter(self, ts_length, init_actions=None):
+        """
+        assessments_sequence = np.empty((ts_length, sum(self.nums_actions)*(self.N-1)))
+        assessments_iter = self.get_time_series_iter(ts_length, init_actions)
+
+        for t, assessments in enumerate(assessments_iter):
+            for i in range(self.N):
+                for j in range(self.N-1):
+                    assessments_sequence[t, self._sum_index(i,j):self._sum_index(i,j+1)] = assessments[i][j]
+
+        assessment_list = []
+        for i in range(self.N):
+            temp = []
+            for j in range(self.N-1):
+                temp.append(np.asarray(assessments_sequence[:, self._sum_index(i,j):self._sum_index(i,j+1)]))
+            assessment_list.append(temp)
+
+        return tuple(assessment_list)
+
+    def get_time_series_iter(self, ts_length, init_actions=None):
+        """
+        Iterater version of `get_time_series` method
+
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            The number of rounds you play in a simulation.
+
+        """
         self.set_init_actions(init_actions)
 
         for t in range(ts_length):
-            yield self.current_beliefs
+            yield self.current_assessments
             self.play()
-            self.update_beliefs(self.step_size(t+1))
+            self.update_asseccements(self.step_size(t+1))
 
-    def replicate(self, T, num_reps, init_actions=None):
+    def iterate_result(self, ts_length, init_actions=None):
         """
-        Returns
-        -------
-        out : tuple(ndarray(float, ndim=2))
+        Returns the ultimate assessments of each players after `ts_length` times play.
+
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            The number of periods you play in a simulation.
 
         """
-        out = np.empty((num_reps, sum(self.nums_actions)))
+        self.set_init_actions(init_actions)
 
-        for j in range(num_reps):
-            beliefs_iter = self.simulate_iter(T+1, init_actions)
-            for beliefs in beliefs_iter:
-                x = beliefs
-            out[j, :self.belief_sizes[0]], out[j, self.belief_sizes[0]:] = x
-
-        return out[:, :self.belief_sizes[0]], out[:, self.belief_sizes[0]:]
+        for t in range(ts_length):
+            self.play()
+            self.update_asseccements(self.step_size(t+1))
+        return self.current_assessments
 
 
 class StochasticFictitiousPlay(FictitiousPlay):
     """
     Stochastic fictitious play with two players.
 
+    Parameters
+    ----------
+    data : array_like(float) or `NormalFormGame` object
+        the payoff matrix of two-player game
+
+    distribution : {'extreme', 'normal'}
+        optional(default='extreme')
+
+    epsilon : scalar(float)
+        optional(default=None)
+
     """
-    def __init__(self, data, distribution='extreme', sigma=1.0, epsilon=None):
+
+    def __init__(self, data, distribution='extreme', epsilon=None):
         FictitiousPlay.__init__(self, data)
 
         if distribution == 'extreme':  # extreme-value, or gumbel, distribution
@@ -152,17 +225,15 @@ class StochasticFictitiousPlay(FictitiousPlay):
             scale = np.sqrt(6) / np.pi
             self.payoff_perturbation_dist = \
                 lambda size: np.random.gumbel(loc=loc, scale=scale, size=size)
-        elif distribution == 'normal':  # normal distribution
-            self.payoff_perturbation_dist = np.random.standard_normal
+        elif distribution == 'normal':  # standard normal distribution
+            self.payoff_perturbation_dist = \
+                lambda size: np.random.standard_normal(size=size)
         else:
-            raise ValueError("distribution must be 'extreme' or 'normal'")
+            raise ValueError("`distribution` must be 'extreme' or 'normal'")
 
-        self.sigma = sigma
+        self._epsilon = epsilon
 
-        # Set step size:
-        # If epsilon is None, step_size = _decreasing_gain,
-        # otherwise, step_size = epsilon
-        self.epsilon = epsilon
+        self.tie_breaking = 'smallest'
 
     @property
     def epsilon(self):
@@ -174,23 +245,28 @@ class StochasticFictitiousPlay(FictitiousPlay):
         self._set_step_size()
 
     def _set_step_size(self):
+        # Set step size:
+        # If epsilon is None, step_size = _decreasing_gain,
+        # otherwise, step_size = epsilon
         if self._epsilon is None:
-            self.step_size = self._decreasing_gain
+            self.step_size = self._initial_weight
         else:
             self.step_size = lambda t: self._epsilon
 
     def play(self):
         """
-
+        The method used to proceed the game by one period. Players take their
+        best response strategies given assessments about opponents actions.
         """
-        n_0, n_1 = self.nums_actions
-        n = n_0 + n_1
+        n = sum(self.nums_actions)
         random_values = self.payoff_perturbation_dist(size=n)
-        payoff_perturbations = (random_values[:n_0], random_values[n_0:])
+        payoff_perturbations = []
+        for j in range(self.N):
+            payoff_perturbations.append(random_values[self._sum_index(j,0):self._sum_index(j+1,0)])
 
         for i, player in enumerate(self.players):
             self.current_actions[i] = player.best_response(
-                player.current_belief,
+                player.current_assessment if self.N > 2 else player.current_assessment[0],
                 tie_breaking=self.tie_breaking,
                 payoff_perturbation=payoff_perturbations[i]
             )
