7903646037b33d4e41d6e48cc0ac08e9c23aeafa
Yuya Furusawa <fullyou0798@gmail.com>
2019-04-01 16:32:43 +0900

    Fix bugs

--- test_fictplay.py
+++ test_fictplay.py
@@ -6,7 +6,8 @@ Tests for fictplay.py
 from __future__ import division
 
 import numpy as np
-from numpy.testing import assert_array_equal, assert_almost_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal, \
+                          assert_almost_equal
 
 from fictplay import FictitiousPlay, StochasticFictitiousPlay
 
@@ -22,17 +23,16 @@ class Test_FictitiousPlay_DecreaingGain:
 
     def test_play(self):
         init_actions = (0, 0)
-        best_responses = [np.asarray([1,0]), np.asarray([1,0])]
+        best_responses = [np.asarray([1,0]), np.asarray([0.5,0.5])]
         assert_array_equal(self.fp.play(init_actions=init_actions), best_responses)
 
     def test_time_series(self):
         x = self.fp.time_series(ts_length=3, init_actions=(0, 0))
-        assert_array_equal(x[0],
+        assert_array_almost_equal(x[0],
             [[1, 0],
              [1, 0],
-             [1, 0]]
-            )
-        assert_array_equal(x[1],
+             [1, 0]])
+        assert_array_almost_equal(x[1],
             [[1,0],
              [1/2,1/2],
              [1/3,2/3]])
@@ -67,7 +67,7 @@ class Test_StochasticFictitiosuPlay_DecreaingGain:
     def setUp(self):
         matching_pennies = [[( 1, -1), (-1,  1)],
                             [(-1,  1), ( 1, -1)]]
-        self.fp = FictitiousPlay(matching_pennies,distribution='extreme')
+        self.fp = StochasticFictitiousPlay(matching_pennies,distribution='extreme')
 
     def test_play(self):
         init_actions = (0, 0)
@@ -78,15 +78,15 @@ class Test_StochasticFictitiosuPlay_DecreaingGain:
     def test_time_series(self):
         x = self.fp.time_series(ts_length=3, init_actions=(0, 0))
         for t in range(3):
-            assert_almost_equal(sum(x[0][:,t]), 1)
-            assert_almost_equal(sum(x[0][:,t]), 1)
+            assert_almost_equal(sum(x[0][t,:]), 1)
+            assert_almost_equal(sum(x[0][t,:]), 1)
 
 class Test_StochasticFictitiosuPlay_ConstantGain:
 
     def setUp(self):
         matching_pennies = [[( 1, -1), (-1,  1)],
                             [(-1,  1), ( 1, -1)]]
-        self.fp = FictitiousPlay(matching_pennies,gain=0.1,distribution='extreme')
+        self.fp = StochasticFictitiousPlay(matching_pennies,gain=0.1,distribution='extreme')
 
     def test_play(self):
         init_actions = (0, 0)
@@ -97,8 +97,8 @@ class Test_StochasticFictitiosuPlay_ConstantGain:
     def test_time_series(self):
         x = self.fp.time_series(ts_length=3, init_actions=(0, 0))
         for t in range(3):
-            assert_almost_equal(sum(x[0][:,t]), 1)
-            assert_almost_equal(sum(x[0][:,t]), 1)
+            assert_almost_equal(sum(x[0][t,:]), 1)
+            assert_almost_equal(sum(x[0][t,:]), 1)
 
 # Invalid inputs #
 
