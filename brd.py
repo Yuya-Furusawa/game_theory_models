c9002a5e5cc4d2167eb6336b0566cf2dfd777cd0
Yuya Furusawa <fullyou0798@gmail.com>
2019-04-03 13:28:10 +0900

    Add random_state

--- brd.py
+++ brd.py
@@ -1,18 +1,32 @@
-"""
-Filename: brd.py
-
-Authors: Daisuke Oyama
-
-Best response dynamics type models.
-
-"""
-from __future__ import division
-
 import numpy as np
-from normal_form_game import Player
+from normal_form_game import *
+from random_game import random_pure_actions
+from util import check_random_state
 
 
-class BRD(object):
+class BRD():
+    """
+    Class representing the best response dynamics model.
+
+    Parameters
+    ----------
+    payoff_matrix : array_like(float, ndim=2)
+        The payoff matrix of the symmetric two-player game.
+
+    N : scalar(int)
+        The number of players.
+
+    Attributes
+    ----------
+    N : scalar(int)
+        The number of players
+
+    num_actions : scalar(int)
+        The number of actions
+
+    player : Player
+        Player instance in the model.
+    """
     def __init__(self, payoff_matrix, N):
         A = np.asarray(payoff_matrix)
         if A.ndim != 2 or A.shape[0] != A.shape[1]:
@@ -23,107 +37,126 @@ class BRD(object):
         self.player = Player(A)  # "Representative player"
         self.tie_breaking = 'smallest'
 
-        # Current action distribution
-        self.current_action_dist = np.zeros(self.num_actions, dtype=int)
-        self.current_action_dist[0] = self.N  # Initialization
+    def _set_action_dist(self, actions):
+        if self.N != len(actions):
+            raise ValueError("length of `actions` must equal to the number of \
+                              players")
+        action_dist = np.zeros(self.num_actions)
+        for i in range(self.N):
+            action_dist[actions[i]] += 1
+        return action_dist
 
-    def set_init_action_dist(self, init_action_dist=None):
-        """
-        Set the attribute `current_action_dist` to `init_action_dist`.
-
-        Parameters
-        ----------
-        init_action_dist : array_like(float, ndim=1),
-                           optional(default=None)
-            Array containing the initial action distribution. If not
-            supplied, randomly chosen uniformly over the set of possible
-            action distributions.
-
-        """
-        if init_action_dist is None:  # Randomly choose an action distribution
-            cutoffs = np.empty(self.num_actions, dtype=int)
-            cutoffs[-1] = self.N + self.num_actions - 1
-            cutoffs[:-1] = np.random.choice(self.N+self.num_actions-1,
-                                            self.num_actions-1, replace=False)
-            cutoffs[:-1].sort()
-            cutoffs[1:] -= cutoffs[:-1] + 1
-            init_action_dist = cutoffs
-        self.current_action_dist[:] = init_action_dist
-
-    def play(self, current_action):
-        self.current_action_dist[current_action] -= 1
-        opponent_action_dist = self.current_action_dist
-        next_action = self.player.best_response(opponent_action_dist,
+    def play(self, action, action_dist):
+        action_dist[action] -= 1
+        next_action = self.player.best_response(action_dist,
                                                 tie_breaking=self.tie_breaking)
-        self.current_action_dist[next_action] += 1
+        action_dist[next_action] += 1
+        return action_dist
 
-    def simulate(self, ts_length, init_action_dist=None):
-        action_dist_sequence = \
-            np.empty((ts_length, self.num_actions), dtype=int)
-        action_dist_sequence_iter = \
-            self.simulate_iter(ts_length, init_action_dist=init_action_dist)
+    def time_series(self, ts_length, init_actions=None, random_state=None):
+        if init_actions is None:
+            nums_actions = tuple([self.num_actions] * self.N)
+            init_actions = random_pure_actions(nums_actions, random_state)
 
-        for t, action_dist in enumerate(action_dist_sequence_iter):
-            action_dist_sequence[t] = action_dist
-
-        return action_dist_sequence
-
-    def simulate_iter(self, ts_length, init_action_dist=None):
-        self.set_init_action_dist(init_action_dist=init_action_dist)
-
-        # Sequence of randomly chosen players to revise
-        player_ind_sequence = np.random.randint(self.N, size=ts_length)
-
+        random_state = check_random_state(random_state)
+        out = np.empty((ts_length, self.num_actions), dtype=int)
+        player_ind_seq = random_state.randint(self.N, size=ts_length)
+        action_dist = self._set_action_dist(init_actions)
         for t in range(ts_length):
-            yield self.current_action_dist
-            action = np.searchsorted(
-                self.current_action_dist.cumsum(), player_ind_sequence[t],
-                side='right'
-            )  # Action the revising player is playing
-            self.play(current_action=action)
-
-    def replicate(self, T, num_reps, init_action_dist=None):
-        out = np.empty((num_reps, self.num_actions), dtype=int)
-
-        for j in range(num_reps):
-            action_dist_sequence_iter = \
-                self.simulate_iter(T+1, init_action_dist=init_action_dist)
-            for action_dist in action_dist_sequence_iter:
-                x = action_dist
-            out[j] = x
-
+            out[t, :] = action_dist[:]
+            action = np.searchsorted(action_dist.cumsum(), player_ind_seq[t],
+                                     side='right')
+            action_dist = self.play(action, action_dist)
         return out
 
 
 class KMR(BRD):
+    """
+    Class representing the Kandori-Mailath-Rob model. Subclass of `BRD`.
+
+    Parameters
+    ----------
+    payoff_matrix : array_like(float, ndim=2)
+        The payoff matrix of the symmetric two-player game.
+
+    N : scalar(int)
+        The number of players.
+
+    epsilon : scalar(float), default=0.1
+        The probability of strategy flips.
+
+    Attributes
+    ----------
+    N : scalar(int)
+        The number of players
+
+    num_actions : scalar(int)
+        The number of actions
+
+    player : Player
+        Player instance in the model.
+
+    epsilon : scalar(float)
+        The probability of strategy flips.
+    """
     def __init__(self, payoff_matrix, N, epsilon=0.1):
         BRD.__init__(self, payoff_matrix, N)
 
         # Mutation probability
         self.epsilon = epsilon
 
-    def play(self, current_action):
-        if np.random.random() < self.epsilon:  # Mutation
-            self.current_action_dist[current_action] -= 1
-            next_action = self.player.random_choice()
-            self.current_action_dist[next_action] += 1
+    def play(self, action, action_dist, random_state=None):
+        random_state = check_random_state(random_state)
+        if random_state.random() < self.epsilon:  # Mutation
+            action_dist[action] -= 1
+            next_action = self.player.random_choice(random_state=random_state)
+            action_dist[next_action] += 1
         else:  # Best response
             BRD.play(self, current_action)
+        return action_dist
 
 
 class SamplingBRD(BRD):
+    """
+    Class representing the sampling BRD model. Subclass of `BRD`.
+
+    Parameters
+    ----------
+    payoff_matrix : array_like(float, ndim=2)
+        The payoff matrix of the symmetric two-player game.
+
+    N : scalar(int)
+        The number of players.
+
+    k : scalar(int), default=2
+        Sample size.
+
+    Attributes
+    ----------
+    N : scalar(int)
+        The number of players
+
+    num_actions : scalar(int)
+        The number of actions
+
+    player : Player
+        Player instance in the model.
+
+    k : scalar(int), default=2
+        Sample size.
+    """
     def __init__(self, payoff_matrix, N, k=2):
         BRD.__init__(self, payoff_matrix, N)
 
         # Sample size
         self.k = k
 
-    def play(self, current_action):
-        self.current_action_dist[current_action] -= 1
-        opponent_action_dist = self.current_action_dist
-        actions = np.random.choice(self.num_actions, size=self.k, replace=True,
-                                   p=opponent_action_dist/(self.N-1))
+    def play(self, action, action_dist, random_state=None):
+        random_state = check_random_state(random_state)
+        action_dist[current_action] -= 1
+        actions = random_state.choice(self.num_actions, size=self.k,
+                                      replace=True, p=action_dist/(self.N-1))
         sample_action_dist = np.bincount(actions, minlength=self.num_actions)
         next_action = self.player.best_response(sample_action_dist,
                                                 tie_breaking=self.tie_breaking)
-        self.current_action_dist[next_action] += 1
+        action_dist[next_action] += 1
