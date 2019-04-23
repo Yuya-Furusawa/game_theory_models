ef7e355a912576961a5314196bc87140f1bc3fbf
Yuya Furusawa <fullyou0798@gmail.com>
2019-04-23 23:09:49 +0900

    Add random number generator in localint.jl

--- test_localint.jl
+++ test_localint.jl
@@ -14,20 +14,23 @@ include("localint.jl")
 @testset "Testing localint.jl" begin
     payoff_matrix = [4 0; 2 3]
     adj_matrix = [0 1 3; 2 0 1; 3 2 0]
-    init_actions = (1,1,2)
+    init_actions = (1, 1, 2)
 
     @testset "Testing local interaction with simultaneous revision" begin
         li = LocalInteraction(payoff_matrix, adj_matrix)
 
-        @test @inferred(play(li,init_actions) == (2,1,1))
-        @test @inferred(time_series(li,3,init_actions) == [1 2 1; 1 1 2; 2 1 2])
+        @test @inferred(play(li, init_actions) == (2, 1, 1))
+        @test @inferred(time_series(li, 3, init_actions) ==
+                        [1 2 1; 1 1 2; 2 1 2])
     end
 
     @testset "Testing local interaction with sequencial revision" begin
         li = LocalInteraction(payoff_matrix, adj_matrix, AsynchronousRevision())
 
-        @test @inferred(play(li,init_actions,1) == (2,1,2))
-        @test @inferred(time_series(li,3,init_actions,[1,2]) ==
+        @test @inferred(play(li, init_actions, 1) == (2,1,2))
+        @test @inferred(time_series(MersenneTwister(1234), li, 3, init_actions
+                        ) == [1 2 2; 1 1 1; 2 2 2])
+        @test @inferred(time_series(li, 3, init_actions, [1,2]) ==
                         [1 2 2; 1 1 2; 2 2 2])
     end
 end
