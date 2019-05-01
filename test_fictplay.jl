afa0c5a42adb3087e7ada547183faf1ff17f81d6
Yuya Furusawa <fullyou0798@gmail.com>
2019-05-01 14:48:37 +0900

    Add vector_approximate_equal and matrix_approximate_equal

--- test_fictplay.jl
+++ test_fictplay.jl
@@ -21,6 +21,31 @@ include("fictplay.jl")
     gain = 0.1
     init_actions = (1,1)
 
+    function vector_approximate_equal(vec1::NTuple{2,Vector{T1}},
+                                      vec2::NTuple{2,Vector{T2}}) where {T1,T2}
+        @test T1 == T2
+        for (x1, x2) in zip(vec1, vec2)
+            @test length(x1) == length(x2)
+            for (xx1, xx2) in zip(x1, x2)
+                @test xx1 ≈ xx2
+            end
+        end
+    end
+
+    function matrix_approximate_equal(mat1::NTuple{2,Matrix{T1}},
+                                      mat2::NTuple{2,Matrix{T2}}) where {T1,T2}
+        @test T1 == T2
+        for (x1, x2) in zip(mat1, mat2)
+            @test size(x1) == size(x2)
+            row, col = size(x1)
+            for i in 1:row
+                for j in 1:col
+                    @test x1[i, j] ≈ x2[i, j]
+                end
+            end
+        end
+    end
+
     @testset "Testing fictitious play" begin
 
         fp_dec = FictitiousPlay(g)
@@ -35,19 +60,10 @@ include("fictplay.jl")
         y_des = ([1.0, 0.0], [0.9, 0.1])
         y_series_des = ([1.0 1.0 1.0; 0.0 0.0 0.0], [1.0 0.9 0.81; 0.0 0.1 0.19])
 
-        for (i, j) in ((1, 1), (1, 2), (2, 1), (2, 2))
-            @test x[i][j] ≈ x_des[i][j]
-            @test y[i][j] ≈ y_des[i][j]
-        end
-
-        for k in 1:2
-            for i in 1:2
-                for j in 1:3
-                    @test x_series[k][i, j] ≈ x_series_des[k][i, j]
-                    @test y_series[k][i, j] ≈ y_series_des[k][i, j]
-                end
-            end
-        end
+        vector_approximate_equal(x, x_des)
+        vector_approximate_equal(y, y_des)
+        matrix_approximate_equal(x_series, x_series_des)
+        matrix_approximate_equal(y_series, y_series_des)
     end
 
     @testset "Testing stochastic fictitious play" begin
@@ -64,21 +80,10 @@ include("fictplay.jl")
         y = [play(MersenneTwister(seed), sfp_con, init_actions) for i in 1:2]
         y_series = [time_series(MersenneTwister(seed), sfp_con, 3, init_actions)
                     for i in 1:2]
-        y_des = ([1.0, 0.0], [0.9, 0.1])
-        y_series_des = ([1.0 1.0 1.0; 0.0 0.0 0.0], [1.0 0.9 0.81; 0.0 0.1 0.19])
 
-        for (i, j) in ((1, 1), (1, 2), (2, 1), (2, 2))
-            @test x[1][i][j] ≈ x[2][i][j]
-            @test y[1][i][j] ≈ y[2][i][j]
-        end
-
-        for k in 1:2
-            for i in 1:2
-                for j in 1:3
-                    @test x_series[1][k][i, j] ≈ x_series[2][k][i, j]
-                    @test y_series[1][k][i, j] ≈ y_series[2][k][i, j]
-                end
-            end
-        end
+        vector_approximate_equal(x[1], x[2])
+        vector_approximate_equal(y[1], y[2])
+        matrix_approximate_equal(x_series[1], x_series[2])
+        matrix_approximate_equal(y_series[1], y_series[2])
     end
 end
